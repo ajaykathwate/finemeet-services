6c7cd0cf81442ae6f76f5934287d2e48b8e71127
Ajay Kathwate <ajaykathwate13@gmail.com>
2025-07-13 19:06:45 +0530

    chore: add user profile create and get endpoints

--- user-service/src/main/java/com/finemeet/userservice/endpoint/UserProfileEndpoint.java
+++ user-service/src/main/java/com/finemeet/userservice/endpoint/UserProfileEndpoint.java
@@ -3,7 +3,9 @@ package com.finemeet.userservice.endpoint;
 import com.finemeet.userservice.dto.UserProfileRequestDto;
 import com.finemeet.userservice.dto.UserProfileResponseDto;
 import com.finemeet.userservice.service.CreateProfileService;
+import com.finemeet.userservice.service.GetUserProfile;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ public class UserProfileEndpoint {
     public static final String USER_SERVICE_ENDPOINT = "/api/users";
 
     private final CreateProfileService createProfileService;
+    private final GetUserProfile getUserProfileService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -23,4 +26,17 @@ public class UserProfileEndpoint {
         UserProfileResponseDto response = createProfileService.createUserProfile(userProfileRequestDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<UserProfileResponseDto> getUserProfile(@PathVariable UUID userId) {
+        UserProfileResponseDto response = getUserProfileService.getUserProfile(userId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "User Service is running!";
+    }
+
 }
