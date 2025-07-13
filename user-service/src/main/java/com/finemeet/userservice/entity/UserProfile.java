36c091bede42c9f0ec5f50fb1ab20c24a41d406c
Ajay Kathwate <ajaykathwate13@gmail.com>
2025-07-13 17:14:08 +0530

    chore: add user and address entities

---
+++ user-service/src/main/java/com/finemeet/userservice/entity/UserProfile.java
@@ -0,0 +1,91 @@
+package com.finemeet.userservice.entity;
+
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Table(name = "user_profiles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserProfile {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", insertable = false)
+    private LocalDateTime updatedAt;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+    /*
+     * Override equals to define logical equality:
+     * Two User objects are considered equal if their 'id' fields are equal.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserProfile user = (UserProfile) o;
+        return new EqualsBuilder()
+                .append(id, user.id)
+                .isEquals();
+    }
+
+    /*
+     * Override hashCode to ensure consistent hashing based on 'id' field,
+     * which is important for using User in hash-based collections like HashSet or HashMap.
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .toHashCode();
+    }
+
+    /*
+     * Override toString for readable object representation,
+     * useful for logging, debugging, and console output.
+     */
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + id + '\'' +
+                '}';
+    }
+}
