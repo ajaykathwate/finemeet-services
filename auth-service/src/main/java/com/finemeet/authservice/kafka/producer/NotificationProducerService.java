01f554c1cd8ce75e000d71b62821f47594197470
Ajay Kathwate <ajaykathwate13@gmail.com>
2025-07-24 23:52:29 +0530

    chore: send to kafka topic

--- auth-service/src/main/java/com/finemeet/authservice/kafka/producer/NotificationProducerService.java
+++ auth-service/src/main/java/com/finemeet/authservice/kafka/producer/NotificationProducerService.java
@@ -1,6 +1,7 @@
 package com.finemeet.authservice.kafka.producer;
 
 import com.finemeet.authservice.kafka.config.KafkaTopicProperties;
+import com.finemeet.authservice.kafka.dto.NotificationEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -13,13 +14,14 @@ public class NotificationProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final KafkaTopicProperties topicProperties;
 
-    public void sendUserRegisteredEvent(String message) {
+    public void sendUserRegisteredEvent(NotificationEvent notificationEvent) {
         String topic = topicProperties.getProducer().getUserRegistered();
         try {
-            kafkaTemplate.send(topic, message).get();
-            log.info("Sent message to [{}] -> data={}", topic, message);
+            kafkaTemplate.send(topic, notificationEvent.getRecipientId(), notificationEvent.toString());
+            log.info("Sent message to [{}] -> data={}", topic, notificationEvent);
         } catch (Exception e) {
             log.error("Failed to send message to topic [{}]: {}", topic, e.getMessage(), e);
+            throw new RuntimeException("Failed to send message to topic [%s]: %s".formatted(topic, e.getMessage()), e);
         }
     }
 }
