dd1b81f1418e5f80b7fe8e513ebd75b80d43455d
Ajay Kathwate <ajaykathwate13@gmail.com>
2025-08-10 01:21:43 +0530

    chore: use topic builder pattern

--- auth-service/src/main/java/com/finemeet/authservice/broker/config/KafkaTopicConfig.java
+++ auth-service/src/main/java/com/finemeet/authservice/broker/config/KafkaTopicConfig.java
@@ -3,6 +3,7 @@ package com.finemeet.authservice.broker.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
@@ -13,8 +14,11 @@ public class KafkaTopicConfig {
     }
 
     @Bean
-    public NewTopic userRegisteredTopic() {
-        return new NewTopic(topicProperties.getProducer().getUserRegistered(), 3, (short) 1);
+    public NewTopic notificationsEventsTopic() {
+        return TopicBuilder.name(topicProperties.getProducer().getNotifications())
+            .partitions(3)
+            .replicas(1)
+            .build();
     }
 
 }
