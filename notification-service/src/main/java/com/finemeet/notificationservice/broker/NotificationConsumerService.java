be3d3e619e9abd1c20481520c82285eb7475006a
Ajay Kathwate <ajaykathwate13@gmail.com>
2025-08-11 00:22:21 +0530

    chore: process notification and refactor file

--- notification-service/src/main/java/com/finemeet/notificationservice/broker/NotificationConsumerService.java
+++ notification-service/src/main/java/com/finemeet/notificationservice/broker/NotificationConsumerService.java
@@ -1,6 +1,7 @@
 package com.finemeet.notificationservice.broker;
 
 import com.finemeet.common.notification.NotificationEvent;
+import com.finemeet.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -14,25 +15,42 @@ import org.springframework.stereotype.Service;
 public class NotificationConsumerService {
 
     private final KafkaTopicProperties kafkaTopicProperties;
+    private final NotificationService notificationService;
 
     @KafkaListener(
             topics = "#{kafkaTopicProperties.notifications}",
             containerFactory = "kafkaListenerContainerFactory"
     )
     public void listen(ConsumerRecord<String, NotificationEvent> consumerRecord, Acknowledgment ack) {
+        log.debug("Kafka listener triggered for topic '{}'", kafkaTopicProperties.getNotifications());
+
         try {
             log.info(
-            "Received message: key={}, value={}, partition={}, offset={}",
-            consumerRecord.key(),
-            consumerRecord.value(),
-            consumerRecord.partition(),
-            consumerRecord.offset());
+                "Received Kafka message: key={}, partition={}, offset={}, value={}",
+                consumerRecord.key(),
+                consumerRecord.partition(),
+                consumerRecord.offset(),
+                consumerRecord.value()
+            );
 
-            log.info("Received notification event: {}", consumerRecord.value());
+            var notificationEvent = consumerRecord.value();
+            notificationService.processNotification(notificationEvent);
 
             ack.acknowledge();
+            log.debug("Kafka message acknowledged: key={}, partition={}, offset={}",
+                      consumerRecord.key(),
+                      consumerRecord.partition(),
+                      consumerRecord.offset());
+
         } catch (Exception e) {
-            log.error("Error processing message", e);
+            log.error(
+                "Error processing Kafka message: key={}, partition={}, offset={}, error={}",
+                consumerRecord.key(),
+                consumerRecord.partition(),
+                consumerRecord.offset(),
+                e.getMessage(),
+                e
+            );
         }
     }
 }
